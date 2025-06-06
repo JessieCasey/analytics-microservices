8edc001580d26722fd9a0c782fd58b7a781e19de
JessieCasey <kenysglobal@gmail.com>
2025-06-07 00:44:55 +0300

    feat: mono app implementation

---
+++ api-service/src/main/java/edu/analytics/DataBaseLoader.java
@@ -0,0 +1,35 @@
+package edu.analytics;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.analytics.prices.model.Price;
+import edu.analytics.prices.repository.PriceRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
+
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
+
+@Slf4j
+@Component
+public class DataBaseLoader {
+
+    @Bean
+    CommandLineRunner initMongo(PriceRepository pricesRepo, ObjectMapper mapper) {
+        return args -> {
+            log.info("Loading prices...");
+            pricesRepo.deleteAll();
+
+            InputStream is = new ClassPathResource("data/initial-data.json").getInputStream();
+            List<Price> prices = Arrays.asList(mapper.readValue(is, Price[].class));
+
+            pricesRepo.saveAll(prices);
+
+            System.out.println("✔️ MongoDB initialized with seed data");
+        };
+    }
+
+}
